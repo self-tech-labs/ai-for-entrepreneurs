e27414f4d537e412bb741413c29a19f644e500d7
Elliot Vaucher <67040832+elliotvaucher@users.noreply.github.com>
2023-11-17 11:50:13 +0100

    Revert "create build"
    This reverts commit f3d12009b985d82b23dbe2ba39e0612138fc426d.

--- out/_next/static/chunks/pages/_error-7caabe9e6b9e3d68.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[64820],{67176:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(90419)}])}},function(n){n.O(0,[49774,92888,40179],function(){return n(n.s=67176)}),_N_E=n.O()}]);
